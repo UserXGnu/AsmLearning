87bec1eb6cc1176fa584e1c51285f83968325563
UserXGnu <userx.gnu@gmail.com>
2016-07-03 22:26:10 -0300

    adding comments

--- x86_64/libc/strlen_test.c
+++ x86_64/libc/strlen_test.c
@@ -1,3 +1,11 @@
+/*
+ * @Note: To compile this:
+ *
+ * as strlen.s -o strlen.o
+ * gcc strlen_test.c -c
+ * gcc strlen.o strlen_test.o -o strlen_test
+ *
+ */
 #include <stdio.h>
 
 #define  HELLO          "HELLO WORLD"
