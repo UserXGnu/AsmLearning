275d5ff3e7695fc718dba1d0164afa332c6e59e8
UserXGnu <userx.gnu@gmail.com>
2016-07-19 19:24:33 -0300

    Adding misc code

---
+++ x86_64/Misc/scas_cmps.s
@@ -0,0 +1,94 @@
+.data
+ 	str1:
+	 	.asciz "Where there's a shell, there's a way!\n"
+	str2:
+	 	.asciz "Where there's a shell, there's a way!\n"
+	equal:
+	 	.asciz "Equal\n"
+	nequal:
+		.asciz "not equal\n"
+
+.text
+ 	.globl  main
+ 	.globl 	compare 
+	.type 	compare, @function
+
+
+main:
+ 	push 	%rbp
+	mov 	%rsp, %rbp
+
+	call 	compare
+ 	cmp 	$0x01, %rax
+	jne 	.Lequal
+ 	je 		.Lnequal
+	
+	.Lmain_end:
+		xor 	%rax, %rax
+		
+		mov 	%rbp, %rsp
+		pop 	%rbp
+		retq
+
+ 	.Lequal:
+	 	mov $equal, %rdi
+		xor %rax, %rax
+		call printf
+		jmp  .Lmain_end
+
+	.Lnequal:
+	 	mov $nequal, %rdi
+		xor %rax, %rax
+		call printf
+		jmp  .Lmain_end 
+
+	 	
+#int compare (const char * src, const char * dst);
+compare:
+ 	push 	%rbp
+	mov 	%rsp, %rbp
+
+
+ 	cld 
+	mov 	$str1, %rdi 
+	call 	len
+	movq 	%rax, %rcx
+	movq 	%rdi, %rsi
+	mov 	$str2, %rdi
+	cld 
+ 	repz  	cmpsb
+	cmp 	$0x00, %rcx
+	jg 		.Lesser
+	
+ 	movq  	$0x00, %rax 
+	
+	.Lend:
+		movq 	%rbp, %rsp
+		pop 	%rbp
+		retq
+
+	.Lesser:
+	 	movq $0x01, %rax
+		jmp 	.Lend
+
+
+
+len:
+ 	push 	%rbp
+	movq 	%rsp, %rbp
+
+	xor 	%rax, %rax
+ 	xor 	%rcx, %rcx
+	not 	%rcx
+ 	
+	repnz  	scasb
+	not 	%rcx
+	movq 	%rcx, %rax
+ 	
+	sub 	%rcx, %rdi
+
+	movq  	%rbp, %rsp
+	pop 	%rbp 
+ 	ret
+
+
